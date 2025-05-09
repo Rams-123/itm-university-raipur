eca10e4abe7f94b1d67a2c6812f8bcb050daddfd
Rams-123 <rams@letsupgrade.in>
2025-05-09 10:23:10 +0530

    add: bba banking & finance program added

---
+++ bba-banking-and-finance/js/swiper.js
@@ -0,0 +1,62 @@
+var swiperOptions2 = {
+  loop: true,
+  spaceBetween: 10,
+  grabCursor: false,
+  slidesPerView: 6,
+  loop: true,
+  autoplay: {
+    delay: 1,
+    disableOnInteraction: true,
+  },
+  noSwiping: true,
+  noSwipingClass: "swiper-slide",
+  speed: 4000,
+  freeModeMomentum: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+};
+
+var swiper = new Swiper(".swiper-container2", swiperOptions2);
+
+
+var swiper1 = new Swiper('.trusted-by-swiper', {
+  spaceBetween: 12,
+  grabCursor: true,
+  a11y: false,
+  freeMode: true,
+  speed: 5000,
+  loop: true,
+  slidesPerView: "auto",
+  noSwiping: true,
+  noSwipingClass: "swiper-slide",
+  autoplay: {
+    delay: 0.5,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      spaceBetween: 12,
+    },
+    480: {
+      spaceBetween: 12,
+    },
+    767: {
+      spaceBetween: 12,
+    },
+    992: {
+      spaceBetween: 12,
+    }
+  },
+});
